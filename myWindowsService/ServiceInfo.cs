c008d4cfe55eff2ee40ebea9ff2cccbf65c78d92
tangmengjin <tangmengjin@laiye.com>
2021-02-03 15:51:20 +0800

    修改一些变量名称和日志

--- myWindowsService/ServiceInfo.cs
+++ myWindowsService/ServiceInfo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace myWindowsService
@@ -17,22 +18,25 @@ namespace myWindowsService
         }
 
         // 主桌面的explorer进程ID
-        int processId = 0;
+        int mainProcessId = 0;
         private ServiceInfo()
         {
             Process[] ps = Process.GetProcessesByName("explorer");
-            if (ps.Length != 1 && 0 == processId)
+            if (ps.Length != 1 && 0 == mainProcessId)
             {
-                Logger.Instance.F(CLASS_NAME, $"explorer count exception, expect 1, got {ps.Length}");
+                Logger.Instance.F(CLASS_NAME, $"explorer count exception, expect 1, got {ps.Length}, exit!!!!");
                 System.Environment.Exit(-1);
             }
-            processId = ps[0].Id;
+            mainProcessId = ps[0].Id;
+            Logger.Instance.D(CLASS_NAME, $"explorer explorer pid={ps[0].Id}," +
+                $" MachineName={ps[0].MachineName}" +
+                $" MainWindowTitle={ps[0].MainWindowTitle}");
         }
 
         // 获得主工作桌面的explorer进程ID
         public int getMainProcessId()
         {
-            return processId;
+            return mainProcessId;
         }
     }
 }
